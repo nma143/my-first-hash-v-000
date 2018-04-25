f7bf4ccf3f40ca53b857239e0530abbb34409e7c
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-04-25 22:53:15 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  nikki = {
+    "age" => "30",
+    "child" =>"middle"
+  }
   
 end
 
